babf7448300b4cacdd13a425cd330da8643da603
Marcos Paulo Ribeiro <31698863+mpr3421account@users.noreply.github.com>
2023-01-10 16:05:11 -0300

    Add project files.

---
+++ Entities/Account.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercExceptions.Entities
+{
+    internal class Account
+    {
+        public int Number { get; set; }
+        public string Holder { get; set; }
+        public double Balance { get; set; }
+        public double WithdrawLimit { get; set; }
+
+        public Account(int number, string holder, double balance, double withdrawLimit)
+        {
+            Number = number;
+            Holder = holder;
+            Balance = balance;
+            WithdrawLimit = withdrawLimit;
+        }
+
+        public void deposit(double amount)
+        {
+            Balance += amount;
+        }
+        public void withdraw(double amount)
+        {
+            Balance -= amount;
+        }
+    }
+}
